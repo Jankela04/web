b949423da3e64c5a8b98a8c2857d77efc76fefb3
Jankela <janezjovanovic04@gmail.com>
2026-04-22 22:34:09 +0200

    h2 config + dummy sql

--- src/main/java/com/rastkela/model/User.java
+++ src/main/java/com/rastkela/model/User.java
@@ -10,6 +10,7 @@ import com.rastkela.enums.UserRole;
 
 
 @Entity
+@Table(name="users")
 public class User implements Serializable{
 
     @Id
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (username, email, password, birth_date, role, blocked)
+ values ('dzankela', 'tempmail123@yes.no', 'pass123', CURRENT_TIMESTAMP, 'USER', false);
+
+INSERT INTO users (username, email, password, birth_date, role, blocked)
+ values ('npc', 'stagod@domain.ok', 'amdin123', '2002-01-03', 'USER', true);
+
+INSERT INTO users (username, email, password, birth_date, registration_date, role, blocked)
+ values ('Rastko', 'arastko@brate.ok', 'nppjmajstor', '2004-01-09', '2022-10-15', 'ADMIN', false);
