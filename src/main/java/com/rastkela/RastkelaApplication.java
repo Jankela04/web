d7fd1c0efaf79c18927b89c7b4b3d28bfa1e1b69
Jankela <janezjovanovic04@gmail.com>
2026-04-22 17:17:38 +0200

    spring init

---
+++ src/main/java/com/rastkela/RastkelaApplication.java
@@ -0,0 +1,13 @@
+package com.rastkela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RastkelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RastkelaApplication.class, args);
+	}
+
+}
