c21ee4666f5b9868b7da6101d10f4a7042c08587
ARastko <ajkalorastko@gmail.com>
2026-05-29 22:01:40 +0200

    zavrseni repozitorijumi

--- src/main/java/com/rastkela/repository/UserRepository.java
+++ src/main/java/com/rastkela/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.rastkela.model.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User,Long>{
 }
