eaadd69df23b72b3d4a710f3a7a90118d8f3fd33
Jankela <janezjovanovic04@gmail.com>
2026-06-02 14:27:25 +0200

    add hashing to password

--- src/main/java/com/rastkela/service/AuthService.java
+++ src/main/java/com/rastkela/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.rastkela.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.rastkela.dto.LoginDto;
@@ -16,9 +17,15 @@ public class AuthService {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public UserSession login(LoginDto loginDto){
         User user = userService.findByUsername(loginDto.getUsername());
-        if(user == null || !user.getPassword().equals(loginDto.getPassword()))
+        if(user == null)
+            return null;
+
+        if(!passwordEncoder.matches(loginDto.getPassword(), user.getPassword()))
             return null;
 
         return new UserSession(user.getId(),user.getUsername(),user.getEmail(),user.getRole());
@@ -29,7 +36,7 @@ public class AuthService {
         user.setBlocked(false);
         user.setUsername(registerDto.getUsername());
         user.setEmail(registerDto.getEmail());
-        user.setPassword(registerDto.getPassword());
+        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
         user.setBirthDate(registerDto.getBirthDate());
         user.setRole(UserRole.USER);
         user.setProfilePicture(registerDto.getProfilePicture());
