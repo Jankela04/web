2102c29c454e6ccab2e2309961f9d80c30193dd3
ARastko <ajkalorastko@gmail.com>
2026-06-03 00:20:56 +0200

    app config ch

--- src/main/java/com/rastkela/AppConfig.java
+++ src/main/java/com/rastkela/AppConfig.java
@@ -2,8 +2,10 @@ package com.rastkela;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class AppConfig {
@@ -11,5 +13,16 @@ public class AppConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/login", "/api/register").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
 }
